c33292825dc10f7ec5d36457905bc8b245f90d2e
Erghauzen <erghauzenderliero@gmail.com>
2024-06-17 10:59:48 +0300

    modified:   src/components/TransferTon.tsx

--- src/components/TransferTon.tsx
+++ src/components/TransferTon.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 import { Address, toNano } from "ton";
 import { useTonConnect } from "../hooks/useTonConnect";
-import { Button, Card, FlexBoxCol, FlexBoxRow, Input } from "./styled/styled";
+import { Button, Card, FlexBoxCol, FlexBoxRow, Input } from "../App";
 import { TonConnect } from "@tonconnect/ui-react";
 
 export function TransferTon() {const { sender, connected } = useTonConnect();
